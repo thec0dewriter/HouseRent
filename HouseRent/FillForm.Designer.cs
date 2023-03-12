6b5a8eff90b50bda73a94198e86c77dbd6af1bf4
Matty <Matty@WORK-LAPTOP>
2023-03-12 17:13:06 +0200

    Add project files.

---
+++ HouseRent/FillForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace HouseRent
+{
+    partial class FillForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvHouses = new System.Windows.Forms.ListView();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.dgvHouses = new System.Windows.Forms.DataGridView();
+            this.houseId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.houseName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.countryId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.countryName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ownerId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ownerName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OwnerEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.capacity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cbCountryFilter = new System.Windows.Forms.ComboBox();
+            this.lbCountryName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHouses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lvHouses
+            // 
+            this.lvHouses.Location = new System.Drawing.Point(42, 426);
+            this.lvHouses.Name = "lvHouses";
+            this.lvHouses.Size = new System.Drawing.Size(642, 264);
+            this.lvHouses.TabIndex = 29;
+            this.lvHouses.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(755, 655);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 35);
+            this.btnExit.TabIndex = 28;
+            this.btnExit.Text = "Kilép";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(426, 64);
+            this.btnFilter.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(112, 35);
+            this.btnFilter.TabIndex = 27;
+            this.btnFilter.Text = "Szűkít";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // dgvHouses
+            // 
+            this.dgvHouses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHouses.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.houseId,
+            this.houseName,
+            this.countryId,
+            this.countryName,
+            this.ownerId,
+            this.ownerName,
+            this.OwnerEmail,
+            this.capacity,
+            this.price});
+            this.dgvHouses.Location = new System.Drawing.Point(42, 118);
+            this.dgvHouses.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvHouses.Name = "dgvHouses";
+            this.dgvHouses.ReadOnly = true;
+            this.dgvHouses.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvHouses.Size = new System.Drawing.Size(825, 277);
+            this.dgvHouses.TabIndex = 26;
+            // 
+            // houseId
+            // 
+            this.houseId.HeaderText = "Id";
+            this.houseId.Name = "houseId";
+            this.houseId.ReadOnly = true;
+            this.houseId.Visible = false;
+            // 
+            // houseName
+            // 
+            this.houseName.HeaderText = "Nev";
+            this.houseName.Name = "houseName";
+            this.houseName.ReadOnly = true;
+            // 
+            // countryId
+            // 
+            this.countryId.HeaderText = "OrszagID";
+            this.countryId.Name = "countryId";
+            this.countryId.ReadOnly = true;
+            this.countryId.Visible = false;
+            // 
+            // countryName
+            // 
+            this.countryName.HeaderText = "Orszag";
+            this.countryName.Name = "countryName";
+            this.countryName.ReadOnly = true;
+            // 
+            // ownerId
+            // 
+            this.ownerId.HeaderText = "TulajID";
+            this.ownerId.Name = "ownerId";
+            this.ownerId.ReadOnly = true;
+            this.ownerId.Visible = false;
+            // 
+            // ownerName
+            // 
+            this.ownerName.HeaderText = "Tulajdonos";
+            this.ownerName.Name = "ownerName";
+            this.ownerName.ReadOnly = true;
+            // 
+            // OwnerEmail
+            // 
+            this.OwnerEmail.HeaderText = "TulajEmail";
+            this.OwnerEmail.Name = "OwnerEmail";
+            this.OwnerEmail.ReadOnly = true;
+            this.OwnerEmail.Visible = false;
+            // 
+            // capacity
+            // 
+            this.capacity.HeaderText = "Ferohely";
+            this.capacity.Name = "capacity";
+            this.capacity.ReadOnly = true;
+            // 
+            // price
+            // 
+            this.price.HeaderText = "Ar";
+            this.price.Name = "price";
+            this.price.ReadOnly = true;
+            // 
+            // cbCountryFilter
+            // 
+            this.cbCountryFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCountryFilter.FormattingEnabled = true;
+            this.cbCountryFilter.Location = new System.Drawing.Point(181, 64);
+            this.cbCountryFilter.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbCountryFilter.Name = "cbCountryFilter";
+            this.cbCountryFilter.Size = new System.Drawing.Size(171, 28);
+            this.cbCountryFilter.TabIndex = 25;
+            // 
+            // lbCountryName
+            // 
+            this.lbCountryName.AutoSize = true;
+            this.lbCountryName.Location = new System.Drawing.Point(38, 67);
+            this.lbCountryName.Name = "lbCountryName";
+            this.lbCountryName.Size = new System.Drawing.Size(98, 20);
+            this.lbCountryName.TabIndex = 32;
+            this.lbCountryName.Text = "Ország neve";
+            // 
+            // FillForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 744);
+            this.Controls.Add(this.lbCountryName);
+            this.Controls.Add(this.lvHouses);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.dgvHouses);
+            this.Controls.Add(this.cbCountryFilter);
+            this.Name = "FillForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FillForm";
+            this.Load += new System.EventHandler(this.FillForm_Load);
+            this.Shown += new System.EventHandler(this.FillForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHouses)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvHouses;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.DataGridView dgvHouses;
+        private System.Windows.Forms.DataGridViewTextBoxColumn houseId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn houseName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn countryId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn countryName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ownerId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ownerName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OwnerEmail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn capacity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price;
+        private System.Windows.Forms.ComboBox cbCountryFilter;
+        private System.Windows.Forms.Label lbCountryName;
+
+    }
+}
---
+++ HouseRent/FillForm.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HouseRent
+{
+    public partial class FillForm : Form
+    {
+        private CountriesDAL countriesDAL;
+        private HousesDAL housesDAL;
+        private string errMess;
+        private int errNumber;
+       
+        public FillForm()
+        {
+            InitializeComponent();
+            string error = string.Empty;
+            housesDAL = new HousesDAL(ref error);
+            if (error != "OK")
+            {
+                errNumber = 1;
+                errMess = "Error"+errNumber+Environment.NewLine+"Houses objektumot nem tudtam letrehozni. " + error;                
+            }
+            else
+            {
+                errMess = "OK";
+                countriesDAL = new CountriesDAL();
+            }
+        }
+
+        private bool first;
+
+        /// <summary>
+        /// This event occurs before a form is displayed for the first time.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FillForm_Load(object sender, EventArgs e)
+        {
+            if (errMess == "OK") //letrejott a Houses objektum
+            {
+                FillCbCountries();
+
+                FillDgvHouses(-1);
+
+                // Inicializaljuk a ListView-t  
+                // Megj: DataGridView eseten konnyebb dolgunk van, 
+                // feluletrol mindezeket a beallitasokat konnyen elvegezhetjuk (lasd. FillForm.cs[Design]), 
+                // majd a DataSource property-jet a feltoltendo adatokat tartalmazo DataSet-re allitjuk
+                InitializeListView();
+                first = true;//ez az elso alkalom, hogy feltoltjuk a ListView-t adatokkal
+                FillLvHouses(-1);
+                first = false;
+                // lblHouseNo.Text = m_Houses.GetHouseNumber().ToString();
+            }            
+        }
+
+        /// <summary>
+        /// Fills the country comboboxes with the country list.
+        /// </summary>
+        /// <param name="combo">specifies which combobox to fill</param>
+        private void FillCbCountries()
+        {
+            string error = string.Empty;
+            List<Country> countryList = countriesDAL.GetCountryList(ref error);
+
+            if (error != "OK")
+            {
+                errNumber++;
+                if (errMess == "OK") 
+                    errMess = string.Empty;
+                errMess = errMess + Environment.NewLine+ 
+                    "Error"+errNumber+Environment.NewLine+"Hiba a ComboBox feltoltesenel." + error;
+            }
+            else
+            {
+                //fill the cbCountryFilter combobox
+                cbCountryFilter.DataSource = countryList;
+                //the text to be dispayed as the combobox text
+                cbCountryFilter.DisplayMember = "CountryName";
+                //the value of the combobox (can be accessed by the selectedValue property)
+                cbCountryFilter.ValueMember = "CountryId";
+            }
+            
+        }
+
+        // Initialize ListView
+        private void InitializeListView()
+        {
+            // Set the view to show details.
+            lvHouses.View = View.Details;
+
+            // Allow the user to edit item text.
+            lvHouses.LabelEdit = true;
+
+            // Allow the user to rearrange columns.
+            lvHouses.AllowColumnReorder = true;
+
+            // Select the item and subitems when selection is made.
+            lvHouses.FullRowSelect = true;
+
+            // Display grid lines.
+            lvHouses.GridLines = true;
+
+            // Sort the items in the list in ascending order.
+            lvHouses.Sorting = SortOrder.Ascending;
+
+            // Attach Subitems to the ListView
+            lvHouses.Columns.Add("NyaraloNev", 100, HorizontalAlignment.Left);
+            lvHouses.Columns.Add("Orszagnev", 100, HorizontalAlignment.Left);
+            lvHouses.Columns.Add("TulajNev", 100, HorizontalAlignment.Left);
+            lvHouses.Columns.Add("Ferohely", 60, HorizontalAlignment.Right);
+            lvHouses.Columns.Add("Ar", 50, HorizontalAlignment.Right);
+            this.Controls.Add(lvHouses);
+        }
+
+        //lvHouses ListView feltoltese adatokkal
+        //adatok = a megadott karakterrel/karakterekkel kezdodo nevu es orszagba tartozo nyaralok
+        //az OrszagID ervenyes kell legyen (countryID>0)
+        private void FillLvHouses(int countryID)
+        {
+            if (!first) //ha nem elso alkalommal toltjuk fel a ListView-t
+            {
+                lvHouses.Clear(); //toroljuk a tartalmat,
+                InitializeListView(); //majd ujra inicializaljuk
+            }
+            string error = string.Empty;
+            List<House> houseList = housesDAL.GetHouseListDataReader(countryID, ref error);
+
+            if ((houseList.Count != 0)&&(error=="OK")) //ha van a felteteleknek eleget tevo nyaralo es nem lepett fel hiba,
+            //akkor feltoltjuk a ListView-t a megfelelo adatokkal
+            {
+                foreach (House item in houseList)
+                {
+                    string[] s = new string[]{item.HouseName, 
+                                              item.HouseCountry.CountryName,
+                                              item.HouseOwner.OwnerName,
+                                              item.Capacity.ToString(), 
+                                              item.Price.ToString()};
+                    ListViewItem lvi = new ListViewItem(s);
+                    lvHouses.Items.Add(lvi);
+                }
+
+                lvHouses.Items[0].Selected = true;
+                lvHouses.Select();
+            }
+            else if (error != "OK")
+            {
+                errNumber++;
+                if (errMess == "OK") errMess = string.Empty;
+                errMess = errMess + Environment.NewLine + 
+                    "Error"+errNumber+Environment.NewLine+"Hiba a ListView feltoltesenel." + error;
+            }
+        }
+
+
+        //dvgHouses DataGridView feltoltese adatokkal
+        //adatok = a megadott karakterrel/karakterekkel kezdodo nevu es orszagba tartozo nyaralok
+        //az OrszagID ervenyes kell legyen (countryID>0)
+        private void FillDgvHouses(int countryID)
+        {
+            string error = string.Empty;
+            dgvHouses.Rows.Clear();
+            List<House> houseList = housesDAL.GetHouseListDataSet(countryID, ref error);
+
+            if ((houseList.Count != 0) && (error == "OK")) //ha van a felteteleknek eleget tevo nyaralo es nem lepett fel hiba,
+            //a lista elemeit hozzaadjuk a DataGridView-hoz(lesznek olyan oszlopok/cellak, melyek 
+            //nem jelennek meg a DataGridView-ban
+            {
+                foreach (House item in houseList)
+                {
+                    try
+                    {
+                        dgvHouses.Rows.Add(item.HouseId,
+                                           item.HouseName,
+                                           item.HouseCountry.CountryId,
+                                           item.HouseCountry.CountryName,
+                                           item.HouseOwner.OwnerId,
+                                           item.HouseOwner.OwnerName,
+                                           item.HouseOwner.OwnerEmail,
+                                           item.Capacity,
+                                           item.Price);
+                    }
+                    catch (Exception ex)
+                    {
+                        errNumber++;
+                        if (errMess == "OK") errMess = string.Empty;
+                        errMess = errMess + Environment.NewLine + 
+                            "Error"+errNumber+Environment.NewLine+"Invalid data " + ex.Message;
+                    }
+                }
+            }
+            else if (error != "OK")
+            {
+                errNumber++;
+                if (errMess == "OK") errMess = string.Empty;
+                errMess = errMess + Environment.NewLine + 
+                    "Error"+errNumber+Environment.NewLine+"Hiba a DataGridView feltoltesenel." + error;
+            }
+        }
+
+        /// <summary>
+        /// Filters the houselist based on the HouseName and the selected countryId 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            //cbCountryFilter.SelectedValue -> countryID
+            //cbCountryFilter.SelectedText -> countryName
+            FillDgvHouses(Convert.ToInt32(cbCountryFilter.SelectedValue));
+            FillLvHouses(Convert.ToInt32(cbCountryFilter.SelectedValue));
+
+            if (errMess != "OK")
+            {
+                ErrorForm errorForm = new ErrorForm(errMess);
+                errorForm.Show();
+                errorForm.Focus();
+            }
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Occurs whenever the form is first shown.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FillForm_Shown(object sender, EventArgs e)
+        {
+            if (errMess != "OK")
+            {
+                ErrorForm errorForm = new ErrorForm(errMess);
+                errorForm.Show();
+                errorForm.Focus();
+            }
+        }     
+
+    }
+}
